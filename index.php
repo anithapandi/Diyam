dc88363c33933b6d7bd7b65c8f78ea497d37c621
anithapandi <anitha@diyaminfotech.com>
2018-10-26 14:36:24 +0530

    master copy

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<head>
+<title>Diyam - Home</title>
+<link rel="stylesheet" href="index.css">
+  <link rel="stylesheet" href="bootstrap.css">
+<style>
+</style>
+</head>
+<body>
+<div id="header">
+<div id="logo_container">
+<img alt="diyam_logo" src="Diyamlogo.jpg" id="diyam_logo">
+</div>
+<ul id="top_nav">
+ <li><a class="active" href="#Home">Home</a></li>
+ <li><a href="#Portfolio">Portfolio</a>
+ 	<ul class="submenu">
+ 		<li><a href="#Portfolio/SPM/ICM">SPM/ICM</a></li>
+ 		<li><a href="#Data Management Services">Data Management Services</a></li>
+ 		<li><a href="# Web Technologies">Web Technologies</a></li>
+ 	</ul>
+ </li>
+ <li><a href="#Customers">Customers</a>
+ 	<ul class="submenu">
+ 		<li><a href="#CCI">Consolidated Communications</a></li>
+ 		<li><a href="#BLR">BlueLine Rental</a></li>
+ 	</ul>
+ </li>
+ <li><a href="#Resources">Resources</a>
+ 	<ul class="submenu">
+ 		<li><a href="#IBM ICM">IBM ICM V10</a></li>
+ 		<li><a href="#KeyPoints">Key Points of ICM</a></li>
+ 		<li><a href="#Videos">Videos</a></li>
+ 		<li><a href="#Blogs">Blogs</a></li>
+ 	</ul>
+ </li>
+ <li><a href="#AboutDiyam">About Diyam</a></li>
+ <li style="padding-right: 0px;"><a href="#ContactUS">Contact Us</a></li>
+</ul>
+</div>
+<div id="container">
+<div id="content">
+	<h1><?php 
+	echo "Our Services....";?></h1>
+</div>
+</div>
+<div id="footer">
+<p> &copy;Diyam Infotech.All Rights reserved.</p>
+</div>
+</body>
+</html>
+
---
+++ timeline.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<head>
+<title>Diyam - Home</title>
+  <link rel="stylesheet" href="bootstrap.css">
+  <link rel="stylesheet" href="tm.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<style>
+</style>
+</head>
+<body>
+	<div class="row">
+		<div class="col-md-6">
+			<ul class="timeline">
+				<li>
+					<a target="_blank" href="index.php">2016</a>
+
+				</li>
+				<li>
+					<a href="#">2017</a>
+				</li>
+				<li>
+					<a href="#">2018</a>
+				</li>
+			</ul>
+		</div>
+		<div class="col-md-6">
+		<h2>Welcome to Diyam!!!</h2>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+		<h1>Welcome to Diyam!!!</h2>
+		</div>
+		</div>
+</body>
+</html>
+
