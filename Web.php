344ede929edcbc0e8bcef6ad1dadb7350f37867b
Mathuniga <44501427+Mathuniga@users.noreply.github.com>
2018-11-08 19:55:43 -0500

    New Changes

---
+++ Web.php
@@ -0,0 +1,28 @@
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="Portfolio.css">
+</head>
+<style>
+
+</style>
+<body>
+<div class="container">
+	<div class="bgWeb">
+	<div class="text-web">
+    <?php 
+       echo "<h1 align=center> “Think Big… Achieve High..And We Make ‘IT’ Possible..”<br>
+       “From Your Idea To Reality…..</h1><br>” ";
+        echo "<h4>A web application is a software application that the user interacts with via a browser. Web applications are growing in popularity because they can be updated and maintained without having to distribute or install software on computers and because of their cross-platform compatibility.
+        At Diyam, We provide innovative software services to ensure our customers are always staying at forefront in technology revolution. We always focus on solving complex problems on web with clear and effective solutions.
+        We cooperate with technology businesses globally to create brand new software products and implement market leading solutions.</h4><br>";
+
+        echo "<h4>This is us..</h4><br>";
+        echo " Yes, It’s true there are lot of companies who do what we do and they also share the same “what” and “how”, but our product consumers work with us for our why(we immediately earn the trust of our clients and exceed their expectation) and our who(individuals with different tastes, aesthetics, opinions and backgrounds)";
+  ?>
+     </div>   	
+  	</div>	
+</div>
+</body>
